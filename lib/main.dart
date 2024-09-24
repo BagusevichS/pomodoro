2ed6d8bc0c747f85e023491fa25f4fdfe7802531
Багусевич Станислав <s.bagusevich@nav-it.ru>
2024-09-24 21:55:15 +0300

    add: имя приложения и цвета системных баров

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pomodoro/presentation/pomodoro_app.dart';
 
 void main() {
   runApp(const PomodoroApp());
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.light,
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.light,
+  ));
 }
